ee4d6f6f28becb9adc1aeaecc761e8abce051916
shanthichalla26 <shanthichalla26@gmail.com>
2020-03-19 18:33:33 -0400

    First commit

---
+++ src/main/java/Cucumber/FrameWork/App.java
@@ -0,0 +1,13 @@
+package Cucumber.FrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/StepDefinations/StepDefination.java
@@ -0,0 +1,43 @@
+package StepDefinations;
+import org.junit.runner.RunWith;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * @author Gamana
+ *
+ */
+@RunWith(Cucumber.class)
+public class StepDefination {
+
+	 @Given("^User is on Netbanking landing page$")
+	    public void user_is_on_netbanking_landing_page() throws Throwable {
+		 System.out.println("user_is_on_netbanking_landing_page");
+	       // throw new PendingException();
+	        
+	    }
+
+	    @When("^User login into application with \"([^\"]*)\" and password \"([^\"]*)\"$")
+	    public void user_login_into_application_with_something_and_password_something(String strArg1, String strArg2) throws Throwable {
+	        //throw new PendingException();
+	    	System.out.println(strArg1);
+	    	System.out.println(strArg2);
+	    	
+	    }
+
+
+	    @Then("^Home page is populated$")
+	    public void home_page_is_populated() throws Throwable {
+	    	 System.out.println("home_page_is_populated");
+	    }
+
+	    @And("^Cards displayed are \"([^\"]*)\"$")
+	    public void cards_displayed_are_something(String strArg1) throws Throwable {
+	       // throw new PendingException();
+	    	System.out.println(strArg1);
+	    }
+}
